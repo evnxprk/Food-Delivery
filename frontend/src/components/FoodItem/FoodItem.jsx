5a977d27c0b5a2c047f7c0aa0096318d57d1e53b
Eunice Park <eunicexpark01@gmail.com>
2024-05-21 20:54:03 -0400

    frontend push

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import { assets } from "../../assets/assets";
 import "./FoodItem.css";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
@@ -14,9 +37,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
           <img className="img" src={assets.rating_starts} />
         </div>
         <p className="food-item-desc">{description}</p>
-        <p className="food-item-price">
-            ${price}
-        </p>
+        <p className="food-item-price">${price}</p>
       </div>
     </div>
   );
